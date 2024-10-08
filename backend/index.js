8f08684a1eeeb44b8e1f3e9d0eec6cc3d5b65075
Milad Coder <miladcoder1390@gmail.com>
2024-10-08 22:00:21 +0330

    Add first route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(234).send('Welcome');
+})
 app.listen(PORT, () => {
     console.log(`App is listening on Port ${PORT}`)
 })
