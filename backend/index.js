c9e2ec4755f27f05dff1673ed7f3576776425c90
Milad Coder <miladcoder1390@gmail.com>
2024-10-08 23:23:35 +0330

    Add an get request for find books by id

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,21 @@ app.get('/books', async (request, response) => {
     }
 })
 
+
+app.get('/books/:id', async (request, response) => {
+    
+    try {
+
+        const { id } = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
 mongoose
     .connect(mongodbURL)
     .then(() => {
