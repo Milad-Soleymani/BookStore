d41cd934a20a622f1b962d296425b6244cd60fc5
Milad Coder <miladcoder1390@gmail.com>
2024-10-08 22:55:12 +0330

    Create post api for add book

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,48 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.status(234).send('Welcome');
 })
 
+// Route for save a new book
+
+app.post("/", async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Send all required fields!",
+            });
+        }
+
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
 
 mongoose
     .connect(mongodbURL)
-    .then(() =>{
+    .then(() => {
         app.listen(PORT, () => {
             console.log(`App is listening on Port ${PORT}`)
         })
