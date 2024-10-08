cafbcc99680d5a099f2e9f42a7134df6cf05be0b
Milad Coder <miladcoder1390@gmail.com>
2024-10-08 23:17:19 +0330

    Add a route for discover all books

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express, { request, response } from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
-
+import { Book } from "./models/bookModel.js";
 const app = express();
 
 app.use(express.json())
@@ -39,6 +39,19 @@ app.post("/", async (request, response) => {
     }
 });
 
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
 
 mongoose
     .connect(mongodbURL)
