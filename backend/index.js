7d2007c22b5e57695f52da24017879ab288e1b25
Milad Coder <miladcoder1390@gmail.com>
2024-10-10 13:16:27 +0330

    add CORS package

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,20 @@
 import express, { request, response } from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
-import booksRoute from './Routes/booksRoute.js'
+import booksRoute from './Routes/booksRoute.js';
+import cors from 'cors';
 
 // ! create an express app
 const app = express();
 
 // ! use a middlewear for parsing json
 app.use(express.json())
+
+app.use(cors({
+    origin: 'https://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders:  ['Content-Type']
+}))
 
 app.use('/books', booksRoute)
 
