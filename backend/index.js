1852f2b0a1d22ce39efd652b48d9840f9700f877
Milad Coder <miladcoder1390@gmail.com>
2024-10-10 11:54:35 +0330

    Add an put request for update books

--- backend/index.js
+++ backend/index.js
@@ -46,22 +46,50 @@ app.get('/books', async (request, response) => {
             count: books.length,
             data: books
         });
-    
+
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
     }
 })
 
+app.put('/books/:id', async (request, response) => {
+    try{
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Send all required fields!",
+            });
+        }
+        const id = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body)
+        
+        if(!result){
+            return response.status(404).json({message: 'Book not found'})
+            return response.status(404).json({message: 'Book not found'})
+        }
+
+        return response.status(200).json({message: 'Book Update Successfully :)'})
+
+    }
+    catch (error)  {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
 
 app.get('/books/:id', async (request, response) => {
-    
+
     try {
 
         const { id } = request.params;
         const book = await Book.findById(id);
         return response.status(200).json(book);
-    
+
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
